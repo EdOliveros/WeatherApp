8bb54d95fb56812db2f8538b60713489d2879dda
Ed Oliveros <edgarolimoy98@gmail.com>
2025-12-28 09:51:42 -0500

    feat: Trying functions to serve info

--- js/ weather-app.js
+++ js/ weather-app.js
@@ -8,9 +8,22 @@ fetch('https://api.openweathermap.org/data/2.5/weather?lat=4.61&lon=-74.08&appid
 // http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
 
 
-function getcity(city) {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q= ${city} &limit=5&appid=6feea6f3fac0889a380448c28f699944`)
-        .then(res => res.json())
-        .then(response => console.log(response[0].lat))
+// function getcity(city) {
+    // fetch(`http://api.openweathermap.org/geo/1.0/direct?q= ${city} &limit=5&appid=6feea6f3fac0889a380448c28f699944`)
+    //         .then(res => res.json())
+    //         .then(response => console.log(response[0].lat))
+    // }
+    
+async function getCity(city) {
+        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q= ${city} &limit=5&appid=6feea6f3fac0889a380448c28f699944`)
+        const data = await response.json()
+        const lat = (data[0].lon)
+        const lon = (data[0].lat)
+        console.log(lat, lon)
 }
- 
+
+async function getWeather(lon, lat) {
+    const response = await fetch(``)
+    const data = response.json()
+
+}
