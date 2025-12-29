10e794fff0dbaa6ab5f8a5db0999b42c99f878cb
Ed Oliveros <edgarolimoy98@gmail.com>
2025-12-29 12:09:03 -0500

    feat: renders functions added

--- js/ weather-app.js
+++ js/ weather-app.js
@@ -26,4 +26,41 @@ async function getWeather(lat, lon) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=6feea6f3fac0889a380448c28f699944&units=metric`)
     const data = await response.json()
     console.log(data)
+    console.log("Pais: " + data.sys.country)
+    console.log("Ciudad: " + data.name)
+    console.log("Temperatura: " + data.main.temp)
+    console.log("Sensacion Termica: " + data.main.feels_like)
+    console.log("Clima: " + data.weather[0].description)
+
+    const details = {Pais:data.sys.country, Ciudad:data.name, Temperatura:data.main.temp, SensacionTermica:data.main.feels_like, Clima:data.weather[0].description}
+
+    renderCiudad(data.name, data.sys.country, data.main.temp, data.weather[0].description)
+    renderDetails(details)
+}
+
+function renderCiudad(ciudad, pais, temperatura, clima) {
+    const location = document.getElementById('location-contenedor')
+    
+    location.innerHTML = `<h3>${ciudad}, ${pais}</h3><h2>${temperatura} °C</h2><h4>${clima}</h4>`;
+}
+
+function renderDetails(d) {
+    const details = document.getElementById('information-contenedor')
+    
+    details.innerHTML = `
+    
+        <div>
+            <p>${d.SensacionTermica} C°</p>
+        </div>
+        <div>
+            <p>${d.Clima} C°</p>
+        </div>
+    
+    `
+}
+
+getCity('Bogota')
+
+function search(city) {
+    getCity(city)
 }
